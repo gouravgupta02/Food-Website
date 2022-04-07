acee142ade619dfbb272040f13debd6238419c4d
Gourav Gupta <gouravguptagg02@gmail.com>
2022-04-07 13:20:55 +0530

    Add content in Food Website

---
+++ foodwebsite.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Food Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Pickup the favourite food item you like.</h1>
+</body>
+</html>
