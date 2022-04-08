ee9cee5a324135d4778434b01a85caf9452886be
Gourav Gupta <gouravguptagg02@gmail.com>
2022-04-08 16:24:40 +0530

    Deleted unnecessary image and added images folder
    made some minor changes in foodwebsite.php

--- foodwebsite.php
+++ foodwebsite.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Food Website</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Pickup the favourite food item you like.</h1>
+    <br>
+    <h1>Street Food: </h1>
+    <br>
+    <h1>Instagram Pages for Food Recipes and more: </h1>
+    <img src="images/Instagram1.png">
 </body>
 </html>
