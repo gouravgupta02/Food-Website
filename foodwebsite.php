85123758a4953e37972ba4d8a70520b70a0bc88b
Gourav Gupta <gouravguptagg02@gmail.com>
2022-04-08 16:59:19 +0530

    Add instagram pages

--- foodwebsite.php
+++ foodwebsite.php
@@ -10,6 +10,10 @@
     <h1>Street Food: </h1>
     <br>
     <h1>Instagram Pages for Food Recipes and more: </h1>
-    <img src="images/Instagram1.png">
-</body>
+    <a href="https://www.instagram.com/picklesandwine/"><img src="images/Instagram1.png"></a>
+    <a href="https://www.instagram.com/carolinagelen/"><img src="images/Instagram2.png"></a>
+    <a href="https://www.instagram.com/spiceology/"><img src="images/Instagram3.png"></a>
+    <a href="https://www.instagram.com/foodnetwork/"><img src="images/Instagram4.png"></a>
+    <a href="https://www.instagram.com/calories.hub/"><img src="images/Instagram5.png"></a>
+    <a href="https://www.instagram.com/tastemade/"><img src="images/Instagram6.png"></a>
 </html>
