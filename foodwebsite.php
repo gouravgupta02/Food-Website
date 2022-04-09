bd5275b924524102818e12050406cd5dc67de153
Gourav Gupta <gouravguptagg02@gmail.com>
2022-04-09 14:26:28 +0530

    Made a minor change

--- foodwebsite.php
+++ foodwebsite.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Food Website</title>
-    <link rel="stylesheet" href="fwebsite.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
-</head>
-<body>
-    <h1>Pickup the favourite food item you like.</h1>
-    <br>
-    <h1>Street Food: </h1>
-    <br>
-    <h1>Instagram Pages of Famous Food Bloggers: </h1>
-    <a href="https://www.instagram.com/food_era_/"><img src="images/Foodb1.png"></a>
-    <a href="https://www.instagram.com/ludhiana_foodiess/"><img src="images/Foodb2.png"></a>
-    <a href="https://www.instagram.com/foodconnectindia/"><img src="images/Foodb3.png"></a>
-    <a href="https://www.instagram.com/chaskasafari/"><img src="images/Foodb4.png"></a>
-    <a href="https://www.instagram.com/foodie_incarnate/"><img src="images/Foodb5.png"></a>
-    <a href="https://www.instagram.com/delhifoodcastle/"><img src="images/Foodb6.png"></a>
-    <a href="https://www.instagram.com/delhi.foodgasm/"><img src="images/Foodb7.png"></a>
-    <a href="https://www.instagram.com/21stcenturymaniacs/"><img src="images/Foodb8.png"></a>
-    <br>
-    <h1>Instagram Pages for Food Recipes: </h1>
-    <a href="https://www.instagram.com/picklesandwine/"><img src="images/Instagram1.png"></a>
-    <a href="https://www.instagram.com/carolinagelen/"><img src="images/Instagram2.png"></a>
-    <a href="https://www.instagram.com/spiceology/"><img src="images/Instagram3.png"></a>
-    <a href="https://www.instagram.com/foodnetwork/"><img src="images/Instagram4.png"></a>
-    <a href="https://www.instagram.com/calories.hub/"><img src="images/Instagram5.png"></a>
-    <a href="https://www.instagram.com/tastemade/"><img src="images/Instagram6.png"></a>
+    <head>
+        <meta charset="UTF-8">
+        <title>Food Website</title>
+        <link rel="stylesheet" href="fwebsite.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Pickup the favourite food item you like.</h1>
+        <br>
+        <h1>Street Food: </h1>
+        <br>
+        <h1>Instagram Pages of Famous Food Bloggers: </h1>
+        <a href="https://www.instagram.com/food_era_/"><img src="images/Foodb1.png"></a>
+        <a href="https://www.instagram.com/ludhiana_foodiess/"><img src="images/Foodb2.png"></a>
+        <a href="https://www.instagram.com/foodconnectindia/"><img src="images/Foodb3.png"></a>
+        <a href="https://www.instagram.com/chaskasafari/"><img src="images/Foodb4.png"></a>
+        <a href="https://www.instagram.com/foodie_incarnate/"><img src="images/Foodb5.png"></a>
+        <a href="https://www.instagram.com/delhifoodcastle/"><img src="images/Foodb6.png"></a>
+        <a href="https://www.instagram.com/delhi.foodgasm/"><img src="images/Foodb7.png"></a>
+        <a href="https://www.instagram.com/21stcenturymaniacs/"><img src="images/Foodb8.png"></a>
+        <br>
+        <h1>Instagram Pages for Food Recipes: </h1>
+        <a href="https://www.instagram.com/picklesandwine/"><img src="images/Instagram1.png"></a>
+        <a href="https://www.instagram.com/carolinagelen/"><img src="images/Instagram2.png"></a>
+        <a href="https://www.instagram.com/spiceology/"><img src="images/Instagram3.png"></a>
+        <a href="https://www.instagram.com/foodnetwork/"><img src="images/Instagram4.png"></a>
+        <a href="https://www.instagram.com/calories.hub/"><img src="images/Instagram5.png"></a>
+        <a href="https://www.instagram.com/tastemade/"><img src="images/Instagram6.png"></a>
+    </body>
 </html>
